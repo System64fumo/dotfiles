17b695b517826ccef89e0da99c07148b65b7f5b8
system64fumo <system64fumo@protonmail.com>
2024-09-09 05:43:11 +0300

    The strongest update
    Happy cirno day! ⑨
    This rice was horribly out of date and needed an update, Today's the day!

--- .config/gtklock/date.sh
+++
@@ -1,4 +0,0 @@
- magick -size 200x50 xc:transparent -font Cantarell -pointsize 24 \
-	-fill grey30 -annotate +20+32 "$(date '+%A %b %d')" -blur 0x2 \
-	-fill lightgrey -annotate +20+30 "$(date '+%A %b %d')" \
-	/tmp/lockscreen-date.png
